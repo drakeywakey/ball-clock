7a2adc6f19f9869d619a15794bc2f8d445574d85
drakeywakey <drakebennion@gmail.com>
2018-08-07 22:07:13 -0600

    green test for moving to hour track after 60 minutes

--- main.go
+++ main.go
@@ -37,13 +37,30 @@ func (c *BallClock) runForMinutes(minutes int) BallClock {
 		c.mainTrack = c.mainTrack[1:]
 
 		if len(c.minuteTrack) == minuteCap {
-			c.fiveTrack = append(c.fiveTrack, ball)
+			// move the 4 balls on the minute track back to the main track
 			for i := minuteCap - 1; i >= 0; i-- {
 				minBall := c.minuteTrack[i]
 				c.minuteTrack = c.minuteTrack[:i]
 				c.mainTrack = append(c.mainTrack, minBall)
 			}
+
+			if len(c.fiveTrack) == fiveCap {
+				// move the 11 balls on the five track back to the main track
+				for i := fiveCap - 1; i >= 0; i-- {
+					fiveBall := c.fiveTrack[i]
+					c.fiveTrack = c.fiveTrack[:i]
+					c.mainTrack = append(c.mainTrack, fiveBall)
+				}
+
+				//add this ball to the hour track
+				c.hourTrack = append(c.hourTrack, ball)
+
+			} else {
+				// add this ball to the five track
+				c.fiveTrack = append(c.fiveTrack, ball)
+			}
 		} else {
+			// add this ball to the minute track
 			c.minuteTrack = append(c.minuteTrack, ball)
 		}
 	}
