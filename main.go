0a0fa153627f623611f9c4473e8932d00c13e1dd
drakeywakey <drakebennion@gmail.com>
2018-08-07 23:16:05 -0600

    refactor and make this command-line ready

--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"math"
+	"os"
 	"reflect"
+	"strconv"
 )
 
 // BallClock indicates which balls are in which time tracks
 type BallClock struct {
-	minuteTrack []int
-	fiveTrack   []int
-	hourTrack   []int
-	mainTrack   []int
+	MinuteTrack []int `json:"Min"`
+	FiveTrack   []int `json:"FiveMin"`
+	HourTrack   []int `json:"Hour"`
+	MainTrack   []int `json:"Main"`
 }
 
 const minuteCap = 4
@@ -18,26 +22,48 @@ const fiveCap = 11
 const hourCap = 11
 
 func newBallClock(numOfBalls int) BallClock {
-	mainTrack := []int{}
+	MainTrack := []int{}
 
 	for i := 1; i <= numOfBalls; i++ {
-		mainTrack = append(mainTrack, i)
+		MainTrack = append(MainTrack, i)
 	}
 
 	return BallClock{
 		[]int{},
 		[]int{},
 		[]int{},
-		mainTrack,
+		MainTrack,
 	}
 }
 
 func main() {
+	// probably a naive decision, but I'm just going to assume they've at least provided a number of balls
+	numOfBalls, err := strconv.Atoi(os.Args[1])
 
+	if err != nil {
+		panic("Sorry, I couldn't understand how many balls you want in the clock")
+	}
+
+	clock := newBallClock(numOfBalls)
+
+	if len(os.Args) == 2 {
+		fmt.Printf("%s balls cycle after %d days", os.Args[1], clock.runUntilRepeat()/(24*60))
+	} else {
+		minutes, err := strconv.Atoi(os.Args[2])
+
+		if err != nil {
+			panic("Sorry, I couldn't understand how many minutes you want to run the clock")
+		}
+
+		clock.runForMinutes(minutes)
+
+		enc := json.NewEncoder(os.Stdout)
+		enc.Encode(clock)
+	}
 }
 
 func (c *BallClock) runUntilRepeat() int {
-	init := newBallClock(len(c.mainTrack))
+	init := newBallClock(len(c.MainTrack))
 	minutes := 1
 	runClockOneMinute(c)
 
@@ -50,53 +76,65 @@ func (c *BallClock) runUntilRepeat() int {
 	return minutes
 }
 
+func (c *BallClock) runForMinutes(minutes int) {
+	for i := 0; i < minutes; i++ {
+		runClockOneMinute(c)
+	}
+}
+
 func runClockOneMinute(c *BallClock) {
-	ball := c.mainTrack[0]
-	c.mainTrack = c.mainTrack[1:]
+	ball := c.MainTrack[0]
+	c.MainTrack = c.MainTrack[1:]
 
-	if len(c.minuteTrack) == minuteCap {
-		// move the 4 balls on the minute track back to the main track
-		for i := minuteCap - 1; i >= 0; i-- {
-			minBall := c.minuteTrack[i]
-			c.minuteTrack = c.minuteTrack[:i]
-			c.mainTrack = append(c.mainTrack, minBall)
-		}
+	if len(c.MinuteTrack) == minuteCap {
 
-		if len(c.fiveTrack) == fiveCap {
-			// move the 11 balls on the five track back to the main track
-			for i := fiveCap - 1; i >= 0; i-- {
-				fiveBall := c.fiveTrack[i]
-				c.fiveTrack = c.fiveTrack[:i]
-				c.mainTrack = append(c.mainTrack, fiveBall)
-			}
+		clearMinuteTrack(c)
 
-			if len(c.hourTrack) == hourCap {
-				// move the 11 balls on the hour track back to the main track
-				for i := hourCap - 1; i >= 0; i-- {
-					hourBall := c.hourTrack[i]
-					c.hourTrack = c.hourTrack[:i]
-					c.mainTrack = append(c.mainTrack, hourBall)
-				}
+		if len(c.FiveTrack) == fiveCap {
 
-				//and move the ball to the main track
-				c.mainTrack = append(c.mainTrack, ball)
+			clearFiveTrack(c)
+
+			if len(c.HourTrack) == hourCap {
+
+				clearHourTrack(c)
+
+				//move the last ball back to the main track
+				c.MainTrack = append(c.MainTrack, ball)
 			} else {
 				//add this ball to the hour track
-				c.hourTrack = append(c.hourTrack, ball)
+				c.HourTrack = append(c.HourTrack, ball)
 			}
 
 		} else {
 			// add this ball to the five track
-			c.fiveTrack = append(c.fiveTrack, ball)
+			c.FiveTrack = append(c.FiveTrack, ball)
 		}
 	} else {
 		// add this ball to the minute track
-		c.minuteTrack = append(c.minuteTrack, ball)
+		c.MinuteTrack = append(c.MinuteTrack, ball)
 	}
 }
 
-func (c *BallClock) runForMinutes(minutes int) {
-	for i := 0; i < minutes; i++ {
-		runClockOneMinute(c)
+func clearMinuteTrack(c *BallClock) {
+	for i := minuteCap - 1; i >= 0; i-- {
+		minBall := c.MinuteTrack[i]
+		c.MinuteTrack = c.MinuteTrack[:i]
+		c.MainTrack = append(c.MainTrack, minBall)
+	}
+}
+
+func clearFiveTrack(c *BallClock) {
+	for i := fiveCap - 1; i >= 0; i-- {
+		fiveBall := c.FiveTrack[i]
+		c.FiveTrack = c.FiveTrack[:i]
+		c.MainTrack = append(c.MainTrack, fiveBall)
+	}
+}
+
+func clearHourTrack(c *BallClock) {
+	for i := hourCap - 1; i >= 0; i-- {
+		hourBall := c.HourTrack[i]
+		c.HourTrack = c.HourTrack[:i]
+		c.MainTrack = append(c.MainTrack, hourBall)
 	}
 }
