2d30edced08241d2d3a7b7dd08f5e3fb3f6ffeab
drakeywakey <drakebennion@gmail.com>
2018-08-07 19:42:57 -0600

    failing test for the fifth ball

--- main_test.go
+++ main_test.go
@@ -18,9 +18,9 @@ func TestNewBallClock(t *testing.T) {
 }
 
 func TestRunTimeFor30Balls(t *testing.T) {
-	clock := newBallClock(30)
 
 	t.Run("it moves 4 balls onto the minute track", func(t *testing.T) {
+		clock := newBallClock(30)
 		got := clock.runForMinutes(4)
 		want := BallClock{
 			[]int{1, 2, 3, 4},
@@ -33,5 +33,20 @@ func TestRunTimeFor30Balls(t *testing.T) {
 			t.Errorf("running the clock should have made %v, but instead made %v", want, got)
 		}
 
+	})
+
+	t.Run("it moves the fifth ball to the fiveTrack, and returns others to the mainTrack", func(t *testing.T) {
+		clock := newBallClock(30)
+		got := clock.runForMinutes(5)
+		want := BallClock{
+			[]int{},
+			[]int{5},
+			[]int{},
+			[]int{6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 4, 3, 2, 1},
+		}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("running the clock should have made %v, but instead made %v", want, got)
+		}
 	})
 }
